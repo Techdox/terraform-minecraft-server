fa56ea376df5c5e597a42d546de94c0a56056f87
Nicholas <33345295+Techdox@users.noreply.github.com>
2019-08-04 16:41:26 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,7 +49,9 @@ resource "google_compute_instance" "default" {
       cd /home/minecraft
       wget https://launcher.mojang.com/v1/objects/f1a0073671057f01aa843443fef34330281333ce/server.jar
       java -Xms1G -Xmx3G -d64 -jar server.jar nogui
-      SCRIPT
+      sed -i 's/false/true/g' eula.txt
+      screen -S mcs java -Xms1G -Xmx3G -d64 -jar server.jar nogui
+      SCRIPTT
   }
 }
 
