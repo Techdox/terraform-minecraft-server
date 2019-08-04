ccad642baa5b4bc2ba68e2b2bc1c065044921dfa
Nicholas <33345295+Techdox@users.noreply.github.com>
2019-08-04 16:26:31 +1200

    fixed double mount

--- main.tf
+++ main.tf
@@ -42,7 +42,6 @@ resource "google_compute_instance" "default" {
   metadata {
       startup-script = <<SCRIPT
       mkdir -p /home/minecraft
-      mount /dev/disk/by-id/google-minecraft-disk /home/minecraft
       mkfs.ext4 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/disk/by-id/google-minecraft-disk
       mount -o discard,defaults /dev/disk/by-id/google-minecraft-disk /home/minecraft
       apt-get update
