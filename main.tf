292ce148d79494daab1e8a4c83026f4eea052c22
Nicholas <33345295+Techdox@users.noreply.github.com>
2019-08-16 20:27:11 +1200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 //Provider
 provider "google" {
-  credentials = "${file("terraform-practice-nlw-c5a6bc11462e.json")}"
   project     = "${var.project_name}"
   region      = "${var.region_name}"
   zone        = "${var.zone_name}"
